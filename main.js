1eb92b124be19c1456228de8f34540f0c07d79ae
nishchal agrawal <80205568+nishchalagrawal@users.noreply.github.com>
2022-06-15 08:32:10 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+musicx = "";
+musicy = "";
+
+function preload(){
+    song = loadsound("music2.mp3")
+    song = loadsound("music.mp3")
+}
+
+function setup(){
+    canvas = createCanvas(600, 500);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.hide();
+}
+
+function draw(){
+image(video,0,0,600,500);
+}
